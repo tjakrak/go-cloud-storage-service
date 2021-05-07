7e4637504a98b04f20f226c84896c3613cb34577
mt-cs <mtania@dons.usfca.edu>
2021-05-06 21:57:04 -0700

    Can read from file. Gob commented out

--- godrive/client/main.go
+++ godrive/client/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"godrive/message"
+	//"godrive/message"
 	"io"
 	"log"
 	"net"
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", ":9997") // connect to localhost port 9999
+	conn, err := net.Dial("tcp", ":9998") // connect to localhost port 9999
 	if err != nil {
 		log.Fatalln(err.Error())
 		return
@@ -19,28 +19,13 @@ func main() {
 	// m := message.Message{Name: "GoDrive"}
 	// fmt.Println(m, something)
 	defer conn.Close()
-	msg := message.New(message.SearchRequest, 300)
-	msg.Print()
-	msg.Send(conn) // pass in our connection
+	// msg := message.New(message.SearchRequest, 300)
+	// msg.Print()
+	// msg.Send(conn) // pass in our connection
 	file, err := os.OpenFile("test.txt", os.O_RDONLY, 0666)
 	if err != nil {
 		log.Fatalln(err.Error())
 		return
 	}
 	io.Copy(conn, file)
-	// scanner := bufio.NewScanner(os.Stdin)
-	// for {
-	// 	fmt.Print("message> ")
-	// 	result := scanner.Scan() // Reads up to a \n newline character
-	// 	if result == false {
-	// 		break
-	// 	}
-
-	// 	message := scanner.Text()
-	// 	if len(message) != 0 {
-	// 		msgBytes := make([]byte, 128) // create a slice w/ 128 byte capacity
-	// 		copy(msgBytes, message)       // won't copy more than 128 bytes
-	// 		conn.Write(msgBytes)
-	// 	}
-	// }
 }
--- godrive/server/main.go
+++ godrive/server/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/gob"
+	//"encoding/gob"
 	"fmt"
-	"godrive/message"
+	//"godrive/message"
 	"io"
 	"log"
 	"net"
@@ -12,38 +12,25 @@ import (
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	decoder := gob.NewDecoder(conn)
-	msg := &message.MessageHeader{}
-	decoder.Decode(msg)
-	fmt.Println(msg)
+	// decoder := gob.NewDecoder(conn)
+	// msg := &message.MessageHeader{}
+	// decoder.Decode(msg)
+	// fmt.Println(msg)
 
-	file, err := os.OpenFile("newfile.txt", os.O_CREATE|os.O_TRUNC|os.O_RDONLY, 0666)
+	file, err := os.OpenFile("newfile.txt", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0666)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	io.Copy(file, conn)
+
+	_, err = io.Copy(file, conn)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	// use io.CopyN for a certain byte for exp header of file
-
-	// for {
-	// 	message := make([]byte, 128)
-	// 	n, err := conn.Read(message)
-	// 	if err != nil {
-	// 		log.Println(err.Error())
-	// 		break
-	// 	}
-	// 	log.Printf("Read %d bytes\n", n)
-
-	// 	if len(message) > 0 {
-	// 		msgStr := string(message)
-	// 		fmt.Println(msgStr)
-	// 	} else {
-	// 		break
-	// 	}
-	// }
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":9997")
+	listener, err := net.Listen("tcp", ":9998")
 	if err != nil {
 		log.Fatalln(err.Error())
 		return
