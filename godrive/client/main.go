128127a645f22fa3b2a08c0b6ef0c4a4edbf6ed8
tjakrak <rgtjakrakartadinata@dons.usfca.edu>
2021-05-12 00:27:15 -0700

    get function working

--- godrive/client/main.go
+++ godrive/client/main.go
@@ -9,42 +9,42 @@ import (
 )
 
 func main() {
-	// commandline processing
-	// path manipulation
 
+    // commandline processing
+	// path manipulation
 	userInput := os.Args
 	fmt.Printf("%s\n", userInput)
 	fmt.Printf("%s\n", userInput[1])
 	fmt.Printf("%d\n", len(userInput))
 
+    // establish tcp connection with the provided ip address by the user
 	conn, err := net.Dial("tcp", userInput[1])
-	// conn, err := net.Dial("tcp", ":9999") // connect to localhost port 9999
 	if err != nil {
 		log.Fatalln(err.Error())
 		return
 	}
+
 	defer conn.Close()
 	var msg *message.Message
 
-	fileStat, err := os.Stat(userInput[3])
-	if err != nil {
-		log.Fatalln(err.Error())
-		return
-	}
-	fileSize := fileStat.Size()
-	log.Printf("File Size: %d\n", fileSize)
-
+    fmt.Printf("userInput[2]")
 	// figure out put or get userInput[2]
 	if userInput[2] == "put" {
+	    fileStat, err := os.Stat(userInput[3])
+	    if err != nil {
+		    log.Fatalln(err.Error())
+		    return
+        }
+        fileSize := fileStat.Size()
+        log.Printf("File Size: %d\n", fileSize)
 		msg = message.New(0, fileSize, userInput[3]) //use os.stat
-		// msg.Send(conn)
 	} else if userInput[2] == "get" {
-		msg = message.New(1, fileSize, userInput[3])
+		msg = message.New(1, 0, userInput[3])
 		// msg.Get(conn)
 	} else if userInput[2] == "search" {
-		msg = message.New(2, fileSize, userInput[3])
+		msg = message.New(2, 0, userInput[3])
 	} else if userInput[2] == "delete" {
-		msg = message.New(3, fileSize, userInput[3])
+		msg = message.New(3, 0, userInput[3])
 	} else {
 		log.Fatalln(err.Error())
 	}
--- godrive/server/main.go
+++ godrive/server/main.go
@@ -1,7 +1,18 @@
 package main
 
+//import (
+//	"godrive/message"
+//	"log"
+//	"net"
+//	"os"
+//)
+
+
 import (
+	"bufio"
+	"encoding/gob"
 	"godrive/message"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -15,19 +26,16 @@ func check(e error) {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	// bconn := bufio.NewReader(conn)
-	// decoder := gob.NewDecoder(bconn)
-	msg := &message.Message{}
-	// err := decoder.Decode(msg)
-	// check(err)
-	// fmt.Println(msg)
 
+	msg := &message.Message{}
+    msg.Head.Size = 31;
 	if _, err := os.Stat("./storage"); err != nil {
 		if os.IsNotExist(err) {
 			err2 := os.Mkdir("./storage", 0755)
 			check(err2)
 		}
 	}
+
 	os.Chdir("./storage")
 	newDir, err := os.Getwd()
 	check(err)
@@ -40,16 +48,38 @@ func handleConnection(conn net.Conn) {
 	// check(err)
 	// log.Printf("New file size: %d\n", bytes)
 	// check(err)
+    msg.Head.Type = 1
 	if msg.Head.Type == 0 {
 		msg.Get(conn)
 	} else {
+        log.Printf("%T\n", conn)
 		msg.Put(conn)
 	}
+
+//    file, err := os.OpenFile("/home/rgtjakrakartadinata/P4-go-away/godrive/storage/test.txt", os.O_RDONLY, 0666)
+//
+//    if err != nil {
+//        log.Fatalln(err.Error())
+//	}
+//
+//	// prefix the send with a size
+//	// create the buffered writer ourselves so gob doesn't do it
+//	bconn := bufio.NewWriter(conn)
+//	encoder := gob.NewEncoder(bconn)
+//	err2 := encoder.Encode(msg)
+//	check(err2)
+//
+//	// open file pass it to io.Copy
+//	sz, err := io.Copy(bconn, file)
+//	check(err)
+//	log.Printf("File size: %d", sz)
+	// ensure all data is written out to the socket
+	bconn.Flush()
 
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "192.168.122.212:9995")
+	listener, err := net.Listen("tcp", "192.168.122.215:9998")
 	if err != nil {
 		log.Fatalln(err.Error())
 		return
